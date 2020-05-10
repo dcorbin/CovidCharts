8fb55674ac96fa500620082e8c099f9d38132399
David Corbin <dave@dcorbin.com>
2020-05-10 14:37:22 -0400

    Don't show line in lengend for when a data is not present

--- src/charting.js
+++ src/charting.js
@@ -1,5 +1,13 @@
 import {StateTable} from './states'
 
+class DataLine {
+    constructor(label, vAxis, color, valueExtractor) {
+        this.label = label
+        this.vAxis = vAxis
+        this.color = color
+        this.valueExtractor = valueExtractor
+    }
+}
 function chart_state_data(covid_data_by_state, state) {
     // Callback that creates and populates a data table,
     // instantiates the pie chart, passes in the data and
@@ -9,8 +17,33 @@ function chart_state_data(covid_data_by_state, state) {
 
     function drawStateCharts() {
         function drawLineChart(targetElementId, lines) {
+            let linesWithData = function () {
+                let linesWithDataOrNull = lines.map(line => {
+                    function hasData(stateData, valueExtractor) {
+                        return stateData.some(r => {
+                            let value = valueExtractor(r);
+                            if (value === null || typeof value === 'undefined' || isNaN(value)) {
+                                return false
+                            }
+                            return true;
+                        })
+                    }
+
+                    let stateDatea = covid_data_by_state[state];
+                    console.log("Has Data check for " + line.label)
+                    let hasData1 = hasData(stateDatea, line.valueExtractor);
+                    if (hasData1) {
+                        return line
+                    }
+                    return null
+                });
+                return linesWithDataOrNull.filter(line => line != null);
+            };
+
+            lines = linesWithData()
+
             let chart_data = covid_data_by_state[state].map(r => {
-                let data =  lines.map(line => line.value_extractor(r))
+                let data =  lines.map(line => line.valueExtractor(r))
                 data.unshift(r.date)
                 return data
             })
@@ -81,25 +114,13 @@ function chart_state_data(covid_data_by_state, state) {
         }
 
 
+        let newPositives7DayAvg = new DataLine('New Positives', 'left', 'blue', r => { return r.seven_day_averages.new_positives });
+        let newHospitalizations7DayAvg = new DataLine('New Hospitalizations', 'right', '#cc9900', r => { return r.seven_day_averages.new_hospitalizations});
+        let newDeaths7DayAvg = new DataLine('New Deaths', 'right', 'red', r => { return r.seven_day_averages.new_deaths});
         drawLineChart('chart', [
-                {
-                    label: 'New Positives',
-                    value_extractor: r => { return r.seven_day_averages.new_positives },
-                    vAxis: 'left',
-                    color: 'blue'
-                },
-                {
-                    label: 'New Hospitalizations',
-                    value_extractor: r => { return r.seven_day_averages.new_hospitalizations },
-                    vAxis: 'right',
-                    color: '#cc9900'
-                },
-                {
-                    label: 'New Deaths',
-                    value_extractor: r => { return r.seven_day_averages.new_deaths },
-                    vAxis: 'right',
-                    color: 'red'
-                }
+                newPositives7DayAvg,
+                newHospitalizations7DayAvg,
+                newDeaths7DayAvg,
             ])
     }
 }
