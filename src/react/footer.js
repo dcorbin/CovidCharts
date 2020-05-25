dce4af77fe92ff8ddb833d0c53d443e00444a0bf
David Corbin <dave@dcorbin.com>
2020-05-25 05:49:15 -0400

    Removed update scheduling

--- src/react/footer.js
+++ src/react/footer.js
@@ -8,13 +8,6 @@ export default function Footer(props) {
             Data sourced from {props.source} &nbsp;&nbsp;
             Report graphing issues at https://github.com/dcorbin/CovidCharts/issues
         </div>
-        <p>
-            Software updates and system maintenance generally happen between 6-8 AM (ET). They are not regular, but
-            happen when
-            a reasonable improvement is available. If you have problems with the site during this time, please be
-            patient and
-            try again later.
-        </p>
     </div>
 }
 
