c6fa3fc7e49b850f0b864de476a0b515e1ed318d
David Corbin <dave@dcorbin.com>
2020-06-02 05:53:16 -0400

    Simplify JSON Map Generation

--- bin/svgMapTranslator.js
+++ bin/svgMapTranslator.js
@@ -3,47 +3,43 @@ let fs = require('fs'),
 
 let baseDir = __dirname + '/..';
 
-function generateGAJson(xml) {
+function normalizeGAXml(xml) {
     function generateLocation(pathElement) {
         let id = pathElement.$.id
-        return `
-    {
-        "name": "${id}",
-        "id": "${id}",
-        "path": "${pathElement.$.d}"
-    }`
+        return {
+                name: id,
+                id: id,
+                path: pathElement.$.d
+        }
     }
 
     let viewBox = "0 0 ${xml.svg.$.width} ${xml.svg.$.height}"
     if (xml.svg.$.viewBox) {
         viewBox = xml.svg.$.viewBox
     }
-    return `{
-    "label" : "Georgia",
-    "viewBox": "${viewBox}",
-    "locations": [ 
-        ${xml.svg.g[0].path.map(generateLocation)} 
-    ]
-}`
+    return {
+        label : "Georgia",
+        viewBox: viewBox,
+        locations: xml.svg.g[0].path.map(generateLocation)
+    }
 }
 
-function generateUSJSon(xml) {
+function normalizeUsXml(xml) {
     function generateLocation(pathElement) {
-        return `
-    {
-        "name": "${pathElement.title[0]}",
-        "id": "${pathElement.$.id}",
-        "path": "${pathElement.$.d}"
-    }`
+        return {
+            name: pathElement.title[0],
+            id: pathElement.$.id,
+            path: pathElement.$.d
+        }
     }
-    return `{
-    "label" : "${xml.svg.title[0]}",
-    "viewBox": "${xml.svg.$.viewBox}",
-    "locations": [ 
-        ${xml.svg.g[0].path.map(generateLocation)} 
-    ]
-}`
+
+    return {
+        label : xml.svg.title[0],
+        viewBox: xml.svg.$.viewBox,
+        locations: xml.svg.g[0].path.map(generateLocation)
+    }
 }
+
 function ensureDirectory(path) {
     path = baseDir + path
     try {
@@ -64,9 +60,10 @@ function convertSvgMap(svgMapProjectPath, generator) {
     let svg = fs.readFileSync(svgMapPath)
     parser.parseString(svg, function (err, result) {
         if (err === null) {
-            fs.writeFileSync(svgJsonPath, generator(result))
+            fs.writeFileSync(svgJsonPath,
+                JSON.stringify(generator(result), null, 4))
         }
     });
 }
-convertSvgMap('/maps/GA.svg', generateGAJson);
-convertSvgMap('/maps/US.svg', generateUSJSon);
+convertSvgMap('/maps/GA.svg', normalizeGAXml);
+convertSvgMap('/maps/US.svg', normalizeUsXml);
