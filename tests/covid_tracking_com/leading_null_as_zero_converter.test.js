2eadc58f008b891854c18b808cd28db382e66db3
David Corbin <dave@dcorbin.com>
2020-06-03 10:48:09 -0400

    Fix test broken in earlier refactoring

--- tests/covid_tracking_com/leading_null_as_zero_converter.test.js
+++ tests/covid_tracking_com/leading_null_as_zero_converter.test.js
@@ -9,17 +9,17 @@ let date4 = new Date(2020, 4, 4);
 const converter = new LeadingNullAsZeroConverter(["foo", "bar"])
 test('aggregation with No Nulls', () => {
     const input = [
-        { date: date3, state: 'AB', foo: 1, bar: 2},
-        { date: date2, state: 'AB', foo: 1, bar: 2},
-        { date: date1, state: 'AB', foo: 1, bar: 2},
+        { date: date3, region: 'AB', foo: 1, bar: 2},
+        { date: date2, region: 'AB', foo: 1, bar: 2},
+        { date: date1, region: 'AB', foo: 1, bar: 2},
     ]
 
     const output = converter.convert(input)
 
     expect(output).toEqual([
-            { date: date1, state: 'AB', foo: 1, bar: 2},
-            { date: date2, state: 'AB', foo: 1, bar: 2},
-            { date: date3, state: 'AB', foo: 1, bar: 2},
+            { date: date1, region: 'AB', foo: 1, bar: 2},
+            { date: date2, region: 'AB', foo: 1, bar: 2},
+            { date: date3, region: 'AB', foo: 1, bar: 2},
         ]
     )
 })
@@ -27,37 +27,37 @@ test('aggregation with No Nulls', () => {
 
 test('aggregation with Nulls', () => {
     const input = [
-        { date: date3, state: 'AB', foo: 1, bar: null},
-        { date: date2, state: 'AB', foo: null, bar: 2},
-        { date: date1, state: 'AB', foo: null, bar: 2},
-        { date: date4, state: 'AB', foo: 1, bar: 2},
+        { date: date3, region: 'AB', foo: 1, bar: null},
+        { date: date2, region: 'AB', foo: null, bar: 2},
+        { date: date1, region: 'AB', foo: null, bar: 2},
+        { date: date4, region: 'AB', foo: 1, bar: 2},
     ]
 
     const output = converter.convert(input)
 
     expect(output).toEqual([
-            { date: date1, state: 'AB', foo: 0, bar: 2},
-            { date: date2, state: 'AB', foo: 0, bar: 2},
-            { date: date3, state: 'AB', foo: 1, bar: null},
-            { date: date4, state: 'AB', foo: 1, bar: 2},
+            { date: date1, region: 'AB', foo: 0, bar: 2},
+            { date: date2, region: 'AB', foo: 0, bar: 2},
+            { date: date3, region: 'AB', foo: 1, bar: null},
+            { date: date4, region: 'AB', foo: 1, bar: 2},
         ]
     )
 })
 test('aggregation with Nulls and MultipleStates', () => {
     const input = [
-        { date: date1, state: 'AB', foo: 1, bar: null},
-        { date: date1, state: 'CD', foo: null, bar: 2},
-        { date: date2, state: 'AB', foo: null, bar: 2},
-        { date: date2, state: 'CD', foo: 1, bar: 2},
+        { date: date1, region: 'AB', foo: 1, bar: null},
+        { date: date1, region: 'CD', foo: null, bar: 2},
+        { date: date2, region: 'AB', foo: null, bar: 2},
+        { date: date2, region: 'CD', foo: 1, bar: 2},
     ]
 
     const output = converter.convert(input)
 
     expect(output).toEqual([
-            { date: date1, state: 'AB', foo: 1, bar: 0},
-            { date: date2, state: 'AB', foo: null, bar: 2},
-            { date: date1, state: 'CD', foo: 0, bar: 2},
-            { date: date2, state: 'CD', foo: 1, bar: 2},
+            { date: date1, region: 'AB', foo: 1, bar: 0},
+            { date: date2, region: 'AB', foo: null, bar: 2},
+            { date: date1, region: 'CD', foo: 0, bar: 2},
+            { date: date2, region: 'CD', foo: 1, bar: 2},
         ]
     )
 })
