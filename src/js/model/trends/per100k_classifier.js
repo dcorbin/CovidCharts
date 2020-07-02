2550cee05975c59c95aa9e716eb09d405d903f2a
David Corbin <dave@dcorbin.com>
2020-07-02 11:09:31 -0400

    Revamp per100k classifier to be NY Relative.

--- src/js/model/trends/per100k_classifier.js
+++ src/js/model/trends/per100k_classifier.js
@@ -1,21 +1,30 @@
 import {createMappingComparator} from "../../util/comparator";
 import Classification from "../../react/model/classification";
+import PositiveGrowthClassifier from "./postiive_growth_classirifer";
 
 const CLASSIFICATIONS = [
-    new Classification("good3", "0 - 1", p => p < 1, 1),
-    new Classification("good2", "1 - 2", p => p < 2, 2),
-    new Classification("good1", "2 - 4", p => p < 4, 3),
-    new Classification("neutral", "4 - 8", p => p < 8, 4),
-    new Classification("bad1", "8 - 16", p => p < 16, 5),
-    new Classification("bad2", "16 - 32", p => p < 32, 6),
-    new Classification("bad3", ">= 32", p => true, 7),
+    new Classification("dataOddity", "Data Anomaly", p => p < 0, 12),
+    new Classification("notApplicable", "N/A", p => isNaN(p), 10),
+    new Classification("allClear", "All Clear", p => p === 0, 9),
+    new Classification("good3", " < ¼ NY", p => p < 25, 8),
+    new Classification("good2", "¼ - ½ NY", p => p < 50, 7),
+    new Classification("good1", "½ - 1 NY", p => p < 100, 6),
+    new Classification("bad1", "1 - 2 NY", p => p < 200, 5),
+    new Classification("bad2", "2 - 4 NY", p => p < 400, 4),
+    new Classification("bad3", ">= 4 NY", p => true, 3),
 ]
 
 export default class Per100KClassifier {
+    constructor() {
+        this.delegate = new PositiveGrowthClassifier()
+    }
     classifications() {
         return [...CLASSIFICATIONS].sort(createMappingComparator(p => p.displayOrder))
     }
     categoryClassName(value, region='Unspecified') {
+        if (isNaN(value)) {
+            return "notApplicable"
+        }
         let classification = CLASSIFICATIONS.find(c => c.requirement(value))
         if (!classification)
         {
@@ -24,6 +33,4 @@ export default class Per100KClassifier {
         }
         return classification.className;
     }
-
-
 }
