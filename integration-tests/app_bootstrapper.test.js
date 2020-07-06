99a8a8bfba403aa89deeff2a932325b55641f7a7
David Corbin <dave@dcorbin.com>
2020-07-06 08:31:49 -0400

    Update AppBootstrapper test

--- integration-tests/app_bootstrapper.test.js
+++ integration-tests/app_bootstrapper.test.js
@@ -1,38 +1,14 @@
 import {beforeEach, expect, jest} from "@jest/globals";
-import DATA_FOCUS_LIST from "../src/js/model/data_focus";
 
-import persistentStateLoader from '../src/js/storage_util'
-jest.mock('../src/js/storage_util')
-
-
-import SettingsStore from "../src/js/settings_store";
-jest.mock('../src/js/settings_store');
-let mockSettingsStore = {
-    load: jest.fn().mockName('SettingsStore#load'),
-    store: jest.fn().mockName('SettingsStore#store')
-}
-SettingsStore.mockImplementation(() => mockSettingsStore)
+import SettingsManager from  '../src/js/settings_manager'
+jest.mock('../src/js/settings_manager' );
 
 import App from '../src/js/app'
-import {Settings} from "../src/js/settings";
-import AppBootstrapper from "../src/js/app_bootstrapper";
 jest.mock('../src/js/app')
+
+import AppBootstrapper from "../src/js/app_bootstrapper";
+import React from "react";
 App.mockReturnValue('DIV')
-
-beforeEach(() => {
-    mockSettingsStore.store.mockClear()
-    mockSettingsStore.load.mockClear()
-});
-
-function invokeIndex() {
-
-    document.body.innerHTML = '<div id="app"/>'
-
-    require("../src/js/index.js")
-    persistentStateLoader.mockReturnValue('covidTracking.com')
-
-    window.onload(new Event("load"))
-}
 
 function assertContentWasRendered() {
     const appDiv = document.getElementById("app")
@@ -46,41 +22,15 @@ function extractProps() {
     return calls[0];
 }
 
-
 test('Test Index when there are settings set', () => {
-    const storedSettings = {
-        'foo': 'bar'
-    }
+    document.body.innerHTML = '<div id="app"/>'
+    const settingsManager = new SettingsManager();
+    const appBootstrapper = new AppBootstrapper(document.getElementById("app"), settingsManager);
 
-    mockSettingsStore.load.mockReturnValue(storedSettings)
-
-    invokeIndex();
+    appBootstrapper.run()
 
     assertContentWasRendered();
     const props = extractProps();
-    expect(props.initialDataFocus).toBe(DATA_FOCUS_LIST[0])
-    expect(props.initialSettings).toMatchObject(storedSettings)
-
-    expect(mockSettingsStore.store.mock.calls.length).toEqual(0)
-
-    const newSettings = {'newSetting': 'alpha'};
-    props.saveSettings(newSettings)
-
-    expect(mockSettingsStore.store.mock.calls).toEqual([[newSettings]])
+    expect(props.settingsManager).toBe(settingsManager)
 })
-//
-// test('Test Index when there are NO settings set', () => {
-//     const defaultSettings = Settings.defaultSettings()
-//
-//     mockSettingsStore.load.mockReturnValue(null)
-//
-//     invokeIndex();
-//
-//     assertContentWasRendered();
-//     const props = extractProps();
-//     expect(props.initialDataFocus).toBe(DATA_FOCUS_LIST[0])
-//     expect(props.initialSettings).toMatchObject(defaultSettings)
-//
-//     expect(mockSettingsStore.store.mock.calls).toEqual([[defaultSettings]])
-// })
 
